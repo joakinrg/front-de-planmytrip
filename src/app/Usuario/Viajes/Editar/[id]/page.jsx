715667a35b028f58bb57782ba68f9f50b90861cc
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-07-01 03:20:21 -0600

    ya edita

--- src/app/Usuario/Viajes/Editar/[id]/page.jsx
+++ src/app/Usuario/Viajes/Editar/[id]/page.jsx
@@ -1,17 +1,145 @@
 "use client"
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getMyTrip, updateMyTrip } from '@/app/api/data/viajes';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+async function fetchMyTrip(id) {
+  try {
+    const res = await getMyTrip(id);
+    console.log("Se obtuvo mi viaje: ", res);
+    return res;
+  } catch (error) {
+    console.error("Error del servidor: No se pudo traer este viaje: ", error);
+  }
+}
 
-function Editar() {
-  const router = useRouter();
-  console.log(router)
+async function sendMyUpdateInfo(formData, id) {
+  try {
+    const res = await updateMyTrip(formData, id)
+    return res
+  } catch (error) {
+    console.error("Error del servidor: No se pudo actualizar los datos: ", error)
+  }
+}
+function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString("es-ES", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+}
+ 
+function Editar({ id }) {
+  const {push} = useRouter()
+  const { data: session, status } = useSession();
+  const [miViaje, setMiViaje] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    titulo: '',
+    fechaInicio: '',
+    fechaRegreso: '',
+    destino: '',
+    descripcion: ''
+  });
+ 
+  useEffect(() => {
+    const myTravel = async () => {
+      if (status === "authenticated") {
+        const viaje = await fetchMyTrip(id);
+        setMiViaje(viaje);
+        setFormData({
+          titulo: viaje.titulo,
+          fechaInicio: viaje.fechaInicio.split('T')[0], // Formateo para el input type date
+          fechaRegreso: viaje.fechaRegreso.split('T')[0], // Formateo para el input type date
+          destino: viaje.destino,
+          descripcion: viaje.descripcion,
+        });
+      }
+    };
+
+    myTravel();
+  }, [session, status]);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí iría la lógica para actualizar los datos del viaje
+
+    const myNewTravel = async () => {
+      const newTravel = await sendMyUpdateInfo(formData, id) 
+      if(newTravel === undefined) {
+        push(`/Usuario/Viajes/Editar/${id}`)
+      }  else {
+        push("/Usuario/Viajes")
+        
+      }
+    }
+    
+myNewTravel()
+    console.log('Datos actualizados:', formData);
+    setIsEditing(false);
+  };
+
   return (
     <div className="relative min-h-screen bg-cover bg-center bg-gray-800 flex items-center justify-center" style={{ backgroundImage: "url('/fondo-mis-viajes.jpg')" }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-8">Editar Viaje</h1>
-        <p className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">ID del Viaje: {id}</p>
-        {/* Aquí puedes agregar el formulario de edición del viaje */}
+        {miViaje ? (
+          <>
+            {!isEditing ? (
+              <div className="bg-gray-700 bg-opacity-70 text-white rounded-lg shadow-lg p-6 max-w-md mx-auto w-96">
+                <p className="mb-2"><strong>Mi título:</strong> {miViaje.titulo}</p>
+                <p className="mb-2"><strong>Fecha de ida:</strong> {formatDate(miViaje.fechaInicio)}</p>
+                <p className="mb-2"><strong>Fecha de regreso:</strong> {formatDate(miViaje.fechaRegreso)}</p>
+                <p className="mb-2"><strong>Destino:</strong> {miViaje.destino}</p>
+                <p className="mb-4"><strong>Descripción:</strong> {miViaje.descripcion}</p>
+                <div className="flex space-x-4 justify-center">
+                  <button onClick={handleEdit} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg">Editar</button>
+                  <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg">Borrar</button>
+                </div>
+              </div>
+            ) : (
+              <form onSubmit={handleSubmit} className="bg-gray-700 bg-opacity-70 text-white rounded-lg shadow-lg p-6 max-w-md mx-auto w-96">
+                <div className="mb-4">
+                  <label className="block text-sm font-semibold mb-2" htmlFor="titulo">Título</label>
+                  <input type="text" id="titulo" name="titulo" value={formData.titulo} onChange={handleChange} className="w-full px-3 py-2 text-gray-800 rounded-lg" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-semibold mb-2" htmlFor="fechaInicio">Fecha de Inicio</label>
+                  <input type="date" id="fechaInicio" name="fechaInicio" value={formData.fechaInicio} onChange={handleChange} className="w-full px-3 py-2 text-gray-800 rounded-lg" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-semibold mb-2" htmlFor="fechaRegreso">Fecha de Regreso</label>
+                  <input type="date" id="fechaRegreso" name="fechaRegreso" value={formData.fechaRegreso} onChange={handleChange} className="w-full px-3 py-2 text-gray-800 rounded-lg" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-semibold mb-2" htmlFor="destino">Destino</label>
+                  <input type="text" id="destino" name="destino" value={formData.destino} onChange={handleChange} className="w-full px-3 py-2 text-gray-800 rounded-lg" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-sm font-semibold mb-2" htmlFor="descripcion">Descripción</label>
+                  <textarea id="descripcion" name="descripcion" value={formData.descripcion} onChange={handleChange} className="w-full px-3 py-2 text-gray-800 rounded-lg"></textarea>
+                </div>
+                <div className="flex space-x-4 justify-center">
+                  <button type="submit" className="bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded-lg">Guardar</button>
+                  <button type="button" onClick={() => setIsEditing(false)} className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg">Cancelar</button>
+                </div>
+              </form>
+            )}
+          </>
+        ) : (
+          <div>Cargando datos...</div>
+        )}
       </div>
     </div>
   );
--- src/app/Usuario/Viajes/page.jsx
+++ src/app/Usuario/Viajes/page.jsx
@@ -1,24 +1,27 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { showMyTrips } from '@/app/api/data/viajes';
-import { useSession } from 'next-auth/react';
-import Link from 'next/link';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import { showMyTrips } from "@/app/api/data/viajes";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import Editar from "./Editar/[id]/page";
 async function fetchMyTrips(email) {
   try {
     const res = await showMyTrips(email);
     console.log("Se obtuvieron los viajes: ", res);
     return res;
   } catch (error) {
-    console.error("Error del servidor: No se pudieron traer los viajes: ", error);
+    console.error(
+      "Error del servidor: No se pudieron traer los viajes: ",
+      error
+    );
   }
 }
 function formatDate(dateStr) {
   const date = new Date(dateStr);
-  return date.toLocaleDateString('es-ES', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
+  return date.toLocaleDateString("es-ES", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
   });
 }
 
@@ -28,7 +31,7 @@ function Viajes() {
 
   useEffect(() => {
     const myTravels = async () => {
-      if (status === 'authenticated') {
+      if (status === "authenticated") {
         const email = session.user.email;
         const viajes = await fetchMyTrips(email);
         setMisViajes(viajes);
@@ -37,56 +40,96 @@ function Viajes() {
 
     myTravels();
   }, [session, status]);
+  if (misViajes === undefined) {
+  } else {
+    const IdViaje = misViajes.id;
 
+    return (
+      <>
+        <Editar id={IdViaje} />
+      </>
+    );
+  }
   return (
-    <div className="relative min-h-screen bg-cover bg-center bg-gray-800 flex items-center justify-center" style={{ backgroundImage: "url('/fondo-mis-viajes.jpg')" }}>
+    <div
+      className="relative min-h-screen bg-cover bg-center bg-gray-800 flex items-center justify-center"
+      style={{ backgroundImage: "url('/fondo-mis-viajes.jpg')" }}
+    >
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-8">Mis Viajes</h1>
-        {misViajes  ? (
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-8">
+          Mis Viajes
+        </h1>
+        {misViajes ? (
           <div className="space-y-4">
-       
-              <div  className="bg-slate-700 bg-opacity-70 text-white rounded-lg shadow-lg p-6 max-w-md mx-auto w-96">
-                <h2 className="text-xl font-bold mb-2">{misViajes.titulo}</h2>
-                <p className="mb-2"><strong>Fecha de inicio:</strong> {formatDate(misViajes.fechaInicio)}</p>
-                <p className="mb-2"><strong>Fecha de regreso:</strong> {formatDate(misViajes.fechaRegreso)}</p>
-                <p className="mb-4"><strong>Destino:</strong> {misViajes.destino}</p>
-             
-                <div className="flex space-x-4 justify-center">
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg">
-                    <a href={`/Usuario/Viajes/Editar/${misViajes.id}`}>Editar Viaje</a></button>
-                  <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg">Eliminar Viaje</button>
-                </div>
+            <div className="bg-slate-700 bg-opacity-70 text-white rounded-lg shadow-lg p-6 max-w-md mx-auto w-96">
+              <h2 className="text-xl font-bold mb-2">{misViajes.titulo}</h2>
+              <p className="mb-2">
+                <strong>Fecha de inicio:</strong>{" "}
+                {formatDate(misViajes.fechaInicio)}
+              </p>
+              <p className="mb-2">
+                <strong>Fecha de regreso:</strong>{" "}
+                {formatDate(misViajes.fechaRegreso)}
+              </p>
+              <p className="mb-4">
+                <strong>Destino:</strong> {misViajes.destino}
+              </p>
+
+              <div className="flex space-x-4 justify-center">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg">
+                  <Link
+                    href="/Usuario/Viajes/Editar/[id].jsx"
+                    as={`/Usuario/Viajes/Editar/${misViajes.id}`}
+                  >
+                    Editar Viaje
+                  </Link>
+                </button>
+                <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg">
+                  Eliminar Viaje
+                </button>
               </div>
-          
+            </div>
           </div>
         ) : (
           <div>
-            <p className="text-3xl md:text-5xl lg:text-7xl font-bold mb-8">Aún no tienes viajes registrados</p>
-            <a href="/Usuario/Viajes/Crear" className="inline-block px-6 py-3 text-xl md:text-2xl bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg">Crear Viaje</a>
+            <p className="text-3xl md:text-5xl lg:text-7xl font-bold mb-8">
+              Aún no tienes viajes registrados
+            </p>
+            <a
+              href="/Usuario/Viajes/Crear"
+              className="inline-block px-6 py-3 text-xl md:text-2xl bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg"
+            >
+              Crear Viaje
+            </a>
           </div>
         )}
-            <a href="/Usuario/Viajes/Crear" className="fixed bottom-8 right-8 px-6 py-3 text-xl md:text-2xl bg-green-600 hover:bg-green-800 text-white font-semibold rounded-full shadow-lg group">
-        <svg
-          className="h-6 w-6 text-white-700"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+        <a
+          href="/Usuario/Viajes/Crear"
+          className="fixed bottom-8 right-8 px-6 py-3 text-xl md:text-2xl bg-green-600 hover:bg-green-800 text-white font-semibold rounded-full shadow-lg group"
         >
-          <path stroke="none" d="M0 0h24v24H0z" />
-          <path
-            d="M15 12h5a2 2 0 0 1 0 4h-15l-3 -6h3l2 2h3l-2 -7h3z"
-            transform="rotate(-15 12 12) translate(0 -1)"
-          />
-          <line x1="3" y1="21" x2="21" y2="21" />
-        </svg>
-        <span className="absolute bottom-full right-1/2 transform translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">Crear Viaje</span>
-      </a>
+          <svg
+            className="h-6 w-6 text-white-700"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path
+              d="M15 12h5a2 2 0 0 1 0 4h-15l-3 -6h3l2 2h3l-2 -7h3z"
+              transform="rotate(-15 12 12) translate(0 -1)"
+            />
+            <line x1="3" y1="21" x2="21" y2="21" />
+          </svg>
+          <span className="absolute bottom-full right-1/2 transform translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+            Crear Viaje
+          </span>
+        </a>
       </div>
     </div>
   );
